335be11dc04322677826e5f875824522bac3de7a
pratchata <ratchata.pyc@gmail.com>
2024-06-19 12:19:55 +0700

    error-middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const errorMiddleware = require("./middleware/error-middleware");
 
 const app = express();
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
